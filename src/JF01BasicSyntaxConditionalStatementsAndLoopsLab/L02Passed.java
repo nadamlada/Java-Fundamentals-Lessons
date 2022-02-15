97fd6475d1736fcfd71e46b8398e0a17bc171635
Nadezhda <nan.dudova@gmail.com>
2022-02-15 12:52:07 +0200

    commit

--- src/JF01BasicSyntaxConditionalStatementsAndLoopsLab/L02Passed.java
+++ src/JF01BasicSyntaxConditionalStatementsAndLoopsLab/L02Passed.java
@@ -1,4 +1,4 @@
-package JFBasicSyntaxConditionalStatementsAndLoopsLab;
+package JF01BasicSyntaxConditionalStatementsAndLoopsLab;
 /*
 Write a program, which takes as an input a grade and prints "Passed!" if the grade is equal or more than 3.00.
 Input
