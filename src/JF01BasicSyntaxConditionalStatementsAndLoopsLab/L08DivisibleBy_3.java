66b8fabe7e03166d7f4af5baed43961fd399f818
Nadezhda <nan.dudova@gmail.com>
2022-02-18 12:53:34 +0200

    commit

--- src/JF01BasicSyntaxConditionalStatementsAndLoopsLab/L08DivisibleBy_3.java
+++ src/JF01BasicSyntaxConditionalStatementsAndLoopsLab/L08DivisibleBy_3.java
@@ -1,4 +1,4 @@
-package JFBasicSyntaxConditionalStatementsAndLoopsLab;
+package JF01BasicSyntaxConditionalStatementsAndLoopsLab;
 
 /*
 Write a program, which prints all the numbers from 1 to 100, which are divisible by 3. You have to use a single for loop. The program should not receive input.
