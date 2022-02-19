cefc029c7b041072f620247a0f2e4d8a9879efef
Nadezhda <nan.dudova@gmail.com>
2022-02-19 12:54:16 +0200

    commit

--- src/JF01BasicSyntaxConditionalStatementsAndLoopsLab/L12EvenNumber.java
+++ src/JF01BasicSyntaxConditionalStatementsAndLoopsLab/L12EvenNumber.java
@@ -1,4 +1,4 @@
-package JFBasicSyntaxConditionalStatementsAndLoopsLab;
+package JF01BasicSyntaxConditionalStatementsAndLoopsLab;
 
 /*
 Take as an input an even number and print its absolute value with a message: "The number is: {absoluteValue}". If the number is odd, print "Please write an even number." and continue reading numbers.
