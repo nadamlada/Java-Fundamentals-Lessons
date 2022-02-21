64bc3983340d2b6dc87d7196205b7b68f1a01d3e
Nadezhda <nan.dudova@gmail.com>
2022-02-21 12:55:19 +0200

    commit

--- src/JF02BasicSyntaxConditionalStatementsAndLoopsExercise/X03Vacation.java
+++ src/JF02BasicSyntaxConditionalStatementsAndLoopsExercise/X03Vacation.java
@@ -1,4 +1,4 @@
-package JFBasicSyntaxConditionalStatementsAndLoopsExercise;
+package JF02BasicSyntaxConditionalStatementsAndLoopsExercise;
 
 /*
 You are given a group of people, type of the group, on which day of the week they are going to stay. Based on that information calculate how much they have to pay and print that price on the console. Use the table below. In each cell is the price for a single person. The output should look like that: "Total price: {price}". The price should be formatted to the second decimal point.
