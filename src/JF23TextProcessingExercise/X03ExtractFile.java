fb47d512f215874a7cfa175c0f1edb7eb7fc5ea5
Nadezhda <nan.dudova@gmail.com>
2022-02-22 08:55:58 +0200

    commit

---
+++ src/JF23TextProcessingExercise/X03ExtractFile.java
@@ -0,0 +1,24 @@
+package JF23TextProcessingExercise;
+
+/*
+Write a program that reads the path to a file and subtracts the file name and its extension.
+ */
+
+import java.util.Scanner;
+
+public class X03ExtractFile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        // \\\\ -> escapes \
+        String[] fullPath = input.split("\\\\");
+        String filenameExtension = fullPath[fullPath.length - 1];
+        // \\. -> escapes .
+        String[] fileNames = filenameExtension.split("\\.");
+        String fileName = fileNames[0];
+        String extension = fileNames[1];
+
+        System.out.printf("File name: %s%n", fileName);
+        System.out.printf("File extension: %s", extension);
+    }
+}
