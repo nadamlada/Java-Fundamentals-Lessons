35d210d579c86b6351db9a21adbb881bdcc9304f
Nadezhda <nan.dudova@gmail.com>
2022-02-26 12:02:12 +0200

    commit

---
+++ src/JF_MoreExerciseBasicSyntaxConditionalStatementsAndLoops/MX04ReverseString.java
@@ -0,0 +1,23 @@
+package JF_MoreExerciseBasicSyntaxConditionalStatementsAndLoops;
+
+/*
+Write a program that reverses a string and prints it on the console.
+ */
+
+import java.util.Scanner;
+
+public class MX04ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        int length = input.length();
+
+
+        for (int i = length - 1; i >= 0; i--) {
+            char currentLetter = input.charAt(length - 1);
+            System.out.print(currentLetter);
+            length = length - 1;
+        }
+    }
+}
