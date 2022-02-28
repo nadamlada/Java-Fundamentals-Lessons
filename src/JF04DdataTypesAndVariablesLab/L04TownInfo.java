3c7b97d74ad62eb81ed5e02aa64a8f2c54705bfa
Nadezhda <nan.dudova@gmail.com>
2022-02-28 12:57:50 +0200

    commit

--- src/JF04DdataTypesAndVariablesLab/L04TownInfo.java
+++ src/JF04DdataTypesAndVariablesLab/L04TownInfo.java
@@ -1,4 +1,4 @@
-package JFDdataTypesAndVariablesLab;
+package JF04DdataTypesAndVariablesLab;
 
 /*
 You will be given 3 lines of input. On the first line you will be given the name of the town, on the second – the population and on the third the area. Use the correct data types and print the result in the following format:
