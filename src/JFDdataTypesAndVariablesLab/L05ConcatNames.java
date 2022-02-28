542afc6fa01e91c224c19c1a3f15f3d811489921
Nadezhda <nan.dudova@gmail.com>
2022-02-28 12:06:34 +0200

    commit

---
+++ src/JFDdataTypesAndVariablesLab/L05ConcatNames.java
@@ -0,0 +1,20 @@
+package JFDdataTypesAndVariablesLab;
+
+/*
+Read two names and a delimiter. Print the names joined by the delimiter.
+ */
+
+import java.util.Scanner;
+
+public class L05ConcatNames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        String nextName = scanner.nextLine();
+        String delimiter = scanner.nextLine();
+
+        System.out.printf("%s%s%s", name, delimiter, nextName);
+
+    }
+}
