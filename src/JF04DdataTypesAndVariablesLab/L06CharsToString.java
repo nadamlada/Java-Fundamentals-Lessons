7f4a76f174ff2510c2973837c482d522e36b0057
Nadezhda <nan.dudova@gmail.com>
2022-03-01 12:58:15 +0200

    commit

--- src/JF04DdataTypesAndVariablesLab/L06CharsToString.java
+++ src/JF04DdataTypesAndVariablesLab/L06CharsToString.java
@@ -1,4 +1,4 @@
-package JFDdataTypesAndVariablesLab;
+package JF04DdataTypesAndVariablesLab;
 
 /*
 Write a program that reads 3 lines of input. On each line, you get a single character. Combine all the characters into one string and print it on the console.
