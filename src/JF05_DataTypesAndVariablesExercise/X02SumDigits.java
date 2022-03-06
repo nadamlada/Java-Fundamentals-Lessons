8950f5e4123fed8f9ebe69da36859334c28c3bd5
Nadezhda <nan.dudova@gmail.com>
2022-03-06 12:40:34 +0200

    commit

---
+++ src/JF05_DataTypesAndVariablesExercise/X02SumDigits.java
@@ -0,0 +1,28 @@
+package JF05_DataTypesAndVariablesExercise;
+
+/*
+You will be given a single integer. Your task is to find the sum of its digits.
+ */
+
+import java.util.Scanner;
+
+public class X02SumDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        int LastDigit = 0;
+
+        while (num > 0) {
+
+            int lastDigit = num % 10;
+            sum += lastDigit;
+            int nextNumber = num / 10;
+            num = nextNumber;
+        }
+
+        System.out.println(sum);
+
+    }
+}
