3052442d9dd293fd3793d1f22c94e7ee1533f512
Nadezhda <nan.dudova@gmail.com>
2022-03-25 13:38:59 +0200

    commit

--- src/JF08ArraysExercise/X01TrainDessi.java
+++ src/JF08ArraysExercise/X01TrainDessi.java
@@ -1,4 +1,4 @@
-package JFArraysExercise;
+package JF08ArraysExercise;
 /*
 You will be given a count of wagons in a train n. On the next n lines, you will receive how many people are going to get on that wagon.
 At the end print the whole train and after that the sum of the people on the train.
