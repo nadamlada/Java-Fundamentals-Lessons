3f8ead4ef8205aad8b14097fddf5a8de7d1020ea
Nadezhda <nan.dudova@gmail.com>
2022-03-27 21:20:18 +0300

    commit

--- src/JF12ListsLab/L07RemoveNegativesAndReverse.java
+++ src/JF12ListsLab/L07RemoveNegativesAndReverse.java
@@ -1,4 +1,4 @@
-package JF13ListsLab;
+package JF12ListsLab;
 
 /*
 Read a list of integers, remove all negative numbers from it and print the remaining elements in reversed order. In case of no elements left in the list, print "empty".
