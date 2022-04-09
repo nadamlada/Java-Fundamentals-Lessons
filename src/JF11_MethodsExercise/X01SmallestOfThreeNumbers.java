d7337cd424e81d3243736bec2f9dec54c5cb0f62
Nadezhda <nan.dudova@gmail.com>
2022-04-09 14:09:00 +0300

    commit

---
+++ src/JF11_MethodsExercise/X01SmallestOfThreeNumbers.java
@@ -0,0 +1,25 @@
+package JF11_MethodsExercise;
+
+/*
+Write a method to print the smallest of three integer numbers. Use an appropriate name for the method.
+ */
+
+import java.util.Scanner;
+
+public class X01SmallestOfThreeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int c = Integer.parseInt(scanner.nextLine());
+
+        int result = smallestOfThreeNumbers(a, b, c);
+        System.out.println(result);
+    }
+
+    private static int smallestOfThreeNumbers(int a, int b, int c) {
+
+        return Math.min(c, Math.min(a, b));
+    }
+}
