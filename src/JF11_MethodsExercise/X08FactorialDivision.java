858442f1a0091b09ff4a5b2667c4256bec647412
Nadezhda <nan.dudova@gmail.com>
2022-04-12 14:15:57 +0300

    commit

---
+++ src/JF11_MethodsExercise/X08FactorialDivision.java
@@ -0,0 +1,39 @@
+package JF11_MethodsExercise;
+
+/*
+Read two integer numbers. Calculate the factorial of each number. Divide the first result by the second and print
+the division formatted to the second decimal point.
+ */
+
+import java.util.Scanner;
+
+public class X08FactorialDivision {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        long factorialA = factorialA(a);
+        long factorialB = factorialA(b);
+        divideAtoB(factorialA, factorialB);
+    }
+
+    // при факториели се работи с лонг
+    private static void divideAtoB(long a, long b) {
+
+        double result = (a * 1.0) / b;
+        System.out.printf("%.2f", result);
+    }
+
+    private static long factorialA(int a) {
+
+        long factorial = a;
+
+        for (int i = 1; i < a; i++) {
+            factorial *= (a - i);
+            // factorial *= i;
+        }
+        return factorial;
+    }
+}
