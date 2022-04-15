5610cdb8987e79850e4765cd56bee16f8ccd8ee7
Nadezhda <nan.dudova@gmail.com>
2022-04-15 14:25:11 +0300

    commit

---
+++ src/JF13ListsLab/L02GaussTrick.java
@@ -0,0 +1,34 @@
+
+package JF13ListsLab;
+
+/*
+Write a program that sum all numbers in a list in the following order:
+first + last, first + 1 + last - 1, first + 2 + last - 2, … first + n, last - n.
+*/
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class L02GaussTrick {
+    public L02GaussTrick() {
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        List<Integer> numbers = (List) Arrays.stream(input.split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+        int size = numbers.size();
+
+        for (int i = 0; i < size / 2; ++i) {
+            int firstNum = (Integer) numbers.get(i);
+            int lastNum = (Integer) numbers.get(numbers.size() - 1);
+            numbers.set(i, firstNum + lastNum);
+            numbers.remove(numbers.size() - 1);
+        }
+
+        System.out.println(numbers.toString().replaceAll("[\\[\\],]", ""));
+    }
+}
