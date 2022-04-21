a78802aa776aae2d592a907455ea26f35aedc799
Nadezhda <nan.dudova@gmail.com>
2022-04-21 14:38:37 +0300

    commit

---
+++ src/JF14ListsExercise/X07AppendArrays.java
@@ -0,0 +1,36 @@
+package JF14ListsExercise;
+
+/*
+Write a program to append several arrays of numbers.
+	Arrays are separated by "|".
+	Values are separated by spaces (" ", one or several).
+	Order the arrays from the last to the first, and their values from left to right.
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class X07AppendArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String thisIs = scanner.nextLine();
+        List<String> input = new ArrayList<>();
+        String[] sth = thisIs.split("\\|");
+
+        for (String s : sth) {
+            input.add(s);
+        }
+
+        Collections.reverse(input);
+
+        System.out.println(input.toString()
+                .replace("[", "")
+                .replace("]", "")
+                .trim()
+                .replaceAll(",", "")
+                .replaceAll("\\s+", " "));
+    }
+}
