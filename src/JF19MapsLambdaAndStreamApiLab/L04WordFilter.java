65bae68364ee85b51b16e6e1194263dc8ad5e37b
Nadezhda <nan.dudova@gmail.com>
2022-05-05 15:20:45 +0300

    commit

---
+++ src/JF19MapsLambdaAndStreamApiLab/L04WordFilter.java
@@ -0,0 +1,18 @@
+package JF19MapsLambdaAndStreamApiLab;
+/*
+Read an array of strings, take only words which length is even. Print each word on a new line.
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class L04WordFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(" ");
+        Arrays.stream(input)
+                .filter(word -> word.length() % 2 == 0)
+                .forEach(word -> System.out.println(word));
+    }
+}
