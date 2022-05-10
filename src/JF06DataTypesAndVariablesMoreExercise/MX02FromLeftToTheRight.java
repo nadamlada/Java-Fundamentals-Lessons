2ed8a37f17527b093add51a67c102ff849986760
Nadezhda <nan.dudova@gmail.com>
2022-05-10 08:57:30 +0300

    commit

--- src/JF06DataTypesAndVariablesMoreExercise/MX02FromLeftToTheRight.java
+++ src/JF06DataTypesAndVariablesMoreExercise/MX02FromLeftToTheRight.java
@@ -1,4 +1,4 @@
-package JF06_DataTypesAndVariablesMoreExercise;
+package JF06DataTypesAndVariablesMoreExercise;
 
 /*
 You will receive a number that represents how many lines we will get as input. On the next N lines, you will receive a string with
@@ -15,39 +15,37 @@ public class MX02FromLeftToTheRight {
         int nan = Integer.parseInt(scanner.nextLine());
 
         for (int p = 1; p <= nan; p++) {
-
             String input = scanner.nextLine();
             int lengthInput = input.length();
-
             String firstNum = "";
+
             int countDigitsFirst = 0;
             int countDigistSecond = 0;
-            for (int i = 0; i < lengthInput; i++) {
 
+            for (int i = 0; i < lengthInput; i++) {
                 char first = input.charAt(i);
+
                 if (first == 32) {                   // сравняване на чарове
                     break;
                 }
+
                 firstNum += first;
                 countDigitsFirst++;
             }
-            //           System.out.println(firstNum);
 
             String secondNum = "";
-            for (int j = countDigitsFirst + 1; j < lengthInput; j++) {
 
+            for (int j = countDigitsFirst + 1; j < lengthInput; j++) {
                 char second = input.charAt(j);
                 secondNum += second;
                 countDigistSecond++;
             }
-            //            System.out.println(secondNum);
 
             double parseFirst = Double.parseDouble(firstNum);
             double parseSecond = Double.parseDouble(secondNum);
-
             int sumFirst = 0;
-
             double firstMathAbs = Math.abs(parseFirst);
+
             while (firstMathAbs > 0) {
                 double digit = firstMathAbs % 10;                             //взима последната цифра
                 sumFirst += digit;
@@ -55,16 +53,14 @@ public class MX02FromLeftToTheRight {
             }
 
             int sumSecond = 0;
-
             double secondMathAbs = Math.abs(parseSecond);
-            while (secondMathAbs > 0) {
 
+            while (secondMathAbs > 0) {
                 double digit = secondMathAbs % 10;                        //взима последната цифра
                 sumSecond += digit;
                 secondMathAbs = secondMathAbs / 10;                       // премахваме последния диджит
-
             }
-            ///// печаташ
+
             if (parseFirst > parseSecond) {
                 System.out.println(sumFirst);
             } else {
