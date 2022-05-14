2550e704c23e64cd6968192ae6a20306a47c88dc
Nadezhda <nan.dudova@gmail.com>
2022-05-14 15:35:36 +0300

    commit

---
+++ src/JF22TextProcessingLab/L01ReverseStrings.java
@@ -0,0 +1,28 @@
+package JF22TextProcessingLab;
+
+/*
+You will be given a series of strings until you receive an "end" command. Write a program that reverses strings and prints
+ each pair on a separate line in the format "{word} = {reversed word}".
+ */
+
+import java.util.Scanner;
+
+public class L01ReverseStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        StringBuilder print = new StringBuilder();
+        while (!text.equals("end")) {
+            for (int i = text.length() - 1; i >= 0; i--) {
+                print.append(text.charAt(i));
+            }
+
+            System.out.printf("%s = ", text);
+            System.out.print(print.toString());
+            System.out.println();
+            print.setLength(0);
+            text = scanner.nextLine();
+        }
+    }
+}
