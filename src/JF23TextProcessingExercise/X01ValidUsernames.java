d7fc0a6aaa072d64ccb5b6f7e9bf8674e9af654a
Nadezhda <nan.dudova@gmail.com>
2022-05-19 15:43:41 +0300

    commit

---
+++ src/JF23TextProcessingExercise/X01ValidUsernames.java
@@ -0,0 +1,41 @@
+package JF23TextProcessingExercise;
+
+/*
+Write a program that reads user names on a single line (joined by ", ") and prints all valid usernames.
+A valid username is:
+•	Has a length between 3 and 16 characters.
+•	Contains only letters, numbers, hyphens, and underscores.
+ */
+
+import java.util.Scanner;
+
+public class X01ValidUsernames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] text = scanner.nextLine().split(", ");
+
+        for (String username : text) {
+            if (isValid(username)) {
+                System.out.println(username);
+            }
+        }
+    }
+
+    private static boolean isValid(String s) {
+
+        if (s.length() < 3 || s.length() > 16) {
+            return false;
+        }
+
+        for (char symbol : s.toCharArray()) {
+            //(!Character.isLetterOrDigit(currentSymbol) && currentSymbol != '-' && currentSymbol != '_')
+            if (!((Character.isLetterOrDigit(symbol))
+                    || (symbol == '_')
+                    || (symbol == '-'))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
