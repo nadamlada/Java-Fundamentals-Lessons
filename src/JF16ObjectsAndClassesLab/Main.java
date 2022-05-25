e2ffd6375eae90d2478d391340f66914a8aeead8
Nadezhda <nan.dudova@gmail.com>
2022-05-25 14:56:19 +0300

    commit

--- src/JF16ObjectsAndClassesLab/Main.java
+++ src/JF16ObjectsAndClassesLab/Main.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class L05Students {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
