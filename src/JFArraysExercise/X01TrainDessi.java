50fc00a585c227f9885242df2698042fe2ce8ee6
Nadezhda <nan.dudova@gmail.com>
2022-05-25 10:41:35 +0300

    commit

---
+++ src/JFArraysExercise/X01TrainDessi.java
@@ -0,0 +1,28 @@
+package JFArraysExercise;
+/*
+You will be given a count of wagons in a train n. On the next n lines, you will receive how many people are going to get on that wagon.
+At the end print the whole train and after that the sum of the people on the train.
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class X01TrainDessi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int wagons = Integer.parseInt(scanner.nextLine());
+
+        int[] peopleArray = new int[wagons];
+
+        for (int i = 0; i < wagons; i++) {
+            int people = Integer.parseInt(scanner.nextLine());
+            peopleArray[i] = people;
+            System.out.print(people + " ");
+
+        }
+        System.out.println();
+        //сумира числата от масива
+        System.out.println(Arrays.stream(peopleArray).sum());
+
+    }
+}
