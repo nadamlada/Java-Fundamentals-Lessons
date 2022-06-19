e63f0eef54ab8b6d00282ce6fa0426a47b355b20
Nadezhda <nan.dudova@gmail.com>
2022-06-19 19:39:08 +0300

    commit

--- src/JF05DataTypesAndVariablesExercise/X07WaterOverflow.java
+++ src/JF05DataTypesAndVariablesExercise/X07WaterOverflow.java
@@ -1,4 +1,4 @@
-package JF05_DataTypesAndVariablesExercise;
+package JF05DataTypesAndVariablesExercise;
 
  /*
  You have a water tank with a capacity of 255 liters.
@@ -30,9 +30,9 @@ public class X07WaterOverflow {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-
         int capacity = 255;
         int sumPoured = 0;
+
         for (int i = 0; i < n; i++) {
             int newLiters = Integer.parseInt(scanner.nextLine());
 
@@ -42,9 +42,8 @@ public class X07WaterOverflow {
             } else {
                 System.out.println("Insufficient capacity!");
             }
-
         }
-        System.out.println(sumPoured);
 
+        System.out.println(sumPoured);
     }
 }
