5d1ccb187eabb18fceef3b343eca04013a5a796c
Nadezhda <nan.dudova@gmail.com>
2022-07-07 14:47:05 +0300

    commit

--- src/JF23TextProcessingLab/L02RepeatStrings.java
+++ src/JF23TextProcessingLab/L02RepeatStrings.java
@@ -1,4 +1,5 @@
-package JF22TextProcessingLab;
+package JF23TextProcessingLab;
+
 /*
 Write a Program That Reads an Array of Strings. Each String is Repeated N Times, Where N is the length of the String. Print the Concatenated String.
  */
