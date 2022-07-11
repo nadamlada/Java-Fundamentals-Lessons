f738d8e11387b56217ca2e228f81af573659958b
Nadezhda <nan.dudova@gmail.com>
2022-07-11 13:48:48 +0300

    commit

--- src/JF25TextProcessingMoreExercise/MX02AsciiSumator.java
+++ src/JF25TextProcessingMoreExercise/MX02AsciiSumator.java
@@ -1,4 +1,5 @@
-package JF24TextProcessingMoreExercise;
+package JF25TextProcessingMoreExercise;
+
 /*
 Write a program that prints a sum of all characters between two given characters (their ASCII code). In the first line,
 you will get a character. On the second line, you get another character. On the last line, you get a random string.
