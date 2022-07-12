01c59821fba2d0d329d110ffc0edbbd1c5060a50
Nadezhda <nan.dudova@gmail.com>
2022-07-12 13:57:44 +0300

    commit

--- src/JF26RegularExpressionsLab/L03MatchDates.java
+++ src/JF26RegularExpressionsLab/L03MatchDates.java
@@ -1,4 +1,15 @@
-package JF25RegularExpressionsLab;
+package JF26RegularExpressionsLab;
+
+/*
+Write a program, which matches a date in the format "dd{separator}MMM{separator}yyyy". Use named capturing groups in your regular expression.
+Compose the Regular Expression
+Every valid date has the following characteristics:
+•	Always starts with two digits, followed by a separator.
+•	After that, it has one uppercase and two lowercase letters (e.g. Jan, Mar).
+•	After that, it has a separator and exactly 4 digits (for the year).
+•	The separator could be either of three things: a period ("."), a hyphen ("-") or a forward-slash ("/").
+•	The separator needs to be the same for the whole date (e.g. 13.03.2016 is valid, 13.03/2016 is NOT). Use a group back reference to check for this.
+ */
 
 import java.util.Scanner;
 import java.util.regex.Matcher;
