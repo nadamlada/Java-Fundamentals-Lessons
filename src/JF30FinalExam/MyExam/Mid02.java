28834a8d23605cca3404149b973935e37c0e6de0
Nadezhda <nan.dudova@gmail.com>
2022-07-20 11:34:56 +0300

    commit

---
+++ src/JF30FinalExam/MyExam/Mid02.java
@@ -0,0 +1,75 @@
+package JF30FinalExam.MyExam;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Mid02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        List<Integer> numbers = Arrays.stream(input.split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+
+        String commands = scanner.nextLine();
+
+        while (!commands.equals("Finish")) {
+            String[] buffer = commands.split(" ");
+
+            switch (buffer[0]) {
+                case "Add":
+                    int value = Integer.parseInt(buffer[1]);
+                    numbers.add(value);
+                    break;
+
+                case "Remove":
+                    int removeThis = Integer.parseInt(buffer[1]);
+
+                    for (int i = 0; i < numbers.size(); i++) {
+
+                        if (numbers.get(i) == removeThis) {
+                            numbers.remove(i);
+                            break;
+                        }
+                    }
+
+                    break;
+
+                case "Replace":
+                    int value1 = Integer.parseInt(buffer[1]);
+                    int value2 = Integer.parseInt(buffer[2]);
+
+                    for (int i = 0; i < numbers.size(); i++) {
+
+                        if (numbers.get(i) == value1) {
+                            numbers.remove(i);
+                            numbers.add(i, value2);
+                            break;
+                        }
+                    }
+
+                    break;
+
+                case "Collapse":
+                    int maxNum = Integer.parseInt(buffer[1]);
+
+                    for (int i = 0; i < numbers.size(); i++) {
+
+                        if (numbers.get(i) < maxNum) {
+                            numbers.remove(i);
+                            i--;
+                        }
+                    }
+
+                    break;
+            }
+            commands = scanner.nextLine();
+
+        }
+
+        String print = numbers.toString().replace("[", "").replace("]", "").replaceAll(",", "");
+        System.out.println(print);
+    }
+}
